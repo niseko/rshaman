2552b927e909e8339f1b9463a4d8546311f7174b
Joscha Rentz <rentz@hotmail.de>
2022-08-21 17:57:13 +0200

    create a script to generate html files from json

---
+++ generateFields.py
@@ -0,0 +1,48 @@
+import json
+import os.path
+import shutil
+from airium import Airium
+
+allowlist = [".\\.git", ".\\Images"]
+subfolders = [ f.path for f in os.scandir() if f.is_dir() ]
+for sf in subfolders:
+    if sf not in allowlist:
+        shutil.rmtree(sf)
+
+def createField(Field, Title, Description, Image, URL):
+    a = Airium()
+    a('<!DOCTYPE html>')
+    with a.head():
+        a.title(_t=Title)
+        a.meta(charset='utf-8')
+        a.meta(content='Resto Title', property='og:title')
+        a.meta(content=Description, property='og:description')
+        a.meta(content=URL, property='og:url')
+        #a.meta(content='Images/'+Image, property='og:image')
+        a.meta(content='0; URL='+URL, **{'http-equiv': 'refresh'})
+        a.link(href=URL, rel='canonical')
+
+    try:
+        os.mkdir(Field)
+    except Exception:
+        pass
+
+    with open(os.path.join(Field, "index.html"), "w") as f:
+        f.write(str(a))
+
+with open("fields.json") as file:
+    data = json.load(file)
+
+for key in data:
+    value = data[key]
+    createField(key, value['Title'], value['Description'], value['Image'], value['URL'])
+    for syn in value['Synonyms']:
+        createField(syn, value['Title'], value['Description'], value['Image'], value['URL'])
+
+
+
+    
+
+
+
+#print (html)
