052e980a84237429736a4554c326485bc6c078f2
Joscha Rentz <rentz@hotmail.de>
2022-08-21 21:30:39 +0200

    bootstrap

--- generateFields.py
+++ generateFields.py
@@ -57,6 +57,7 @@ with a.head():
     a.meta(property='og:url', content="rshaman.com")
     a.meta(property='og:image', content='https://niseko.github.io/rshaman/images/aglogo.png')
     a.meta(content='#0070DD', name="theme-color")
+    a.link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx", crossorigin="anonymous")
 with a.body():
     with a.ul():
         for key in data:
